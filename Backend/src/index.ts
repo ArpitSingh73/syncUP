b8253897fa8ced17438690e0e668de87a17998ea
ArpitSingh73 <arpitsingh73073@gmail.com>
2025-02-12 11:10:55 +0530

    handle errors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,7 +3,11 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import session from "cookie-session";
 import { config } from "./config/app.config";
-
+ import connectDatabase  from "./config/database.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+import { HTTPSTATUS } from "./config/http.config";
 const app = express();
 
 app.use(express.json());
@@ -30,23 +34,19 @@ app.use(
 );
 
 app.get(
-    `/`, (req: Request, res: Response) => { 
-        res.send('Hello World');
-        console.log('Hello World');
-    }
-//   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-//     throw new BadRequestException(
-//       "This is a bad request",
-//       ErrorCodeEnum.AUTH_INVALID_TOKEN
-//     );
-//     return res.status(HTTPSTATUS.OK).json({
-//       message: "Hello Subscribe to the channel & share",
-//     });
-//   })
+    `/`,   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException(
+      "This is a bad request",
+      ErrorCodeEnum.AUTH_INVALID_TOKEN
+    );
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subscribe to the channel & share",
+    });
+  })
 );
 
 
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
-//   await connectDatabase();
+  await connectDatabase();
 });
